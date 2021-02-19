f7a62c7eac9c325553a7e9e9a2c5054ed875d01d
Thiago Freitas <tfreitasdevv@gmail.com>
2021-02-19 17:37:25 -0300

    Rest testado

---
+++ src/main/java/com/tmidevelopment/saolourenco/resources/EstadoResource.java
@@ -0,0 +1,16 @@
+package com.tmidevelopment.saolourenco.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/estados")
+public class EstadoResource {
+	
+	@GetMapping
+	public String listar() {
+		return "REST está funcionando";
+	}
+
+}
