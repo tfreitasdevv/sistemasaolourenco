b6fa6150d719c9a3598089315bb009403ee874c4
Thiago Freitas <tfreitasdevv@gmail.com>
2021-02-19 17:50:12 -0300

    Criado basicamente o domain Estado

--- src/main/java/com/tmidevelopment/saolourenco/resources/EstadoResource.java
+++ src/main/java/com/tmidevelopment/saolourenco/resources/EstadoResource.java
@@ -1,16 +1,29 @@
 package com.tmidevelopment.saolourenco.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tmidevelopment.saolourenco.domain.Estado;
+
 @RestController
 @RequestMapping(value = "/estados")
 public class EstadoResource {
-	
+
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando";
+	public List<Estado> listar() {
+
+		Estado e1 = new Estado(1, "RJ");
+		Estado e2 = new Estado(1, "SP");
+
+		List<Estado> lista = new ArrayList<>();
+		lista.add(e1);
+		lista.add(e2);
+
+		return lista;
 	}
 
 }
