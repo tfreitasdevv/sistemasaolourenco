227077abd57956d27574c9e1356fc820d34a81fd
Thiago Freitas <tfreitasdevv@gmail.com>
2021-02-19 17:27:01 -0300

    Projeto criado

---
+++ src/main/java/com/tmidevelopment/saolourenco/SaolourencoApplication.java
@@ -0,0 +1,13 @@
+package com.tmidevelopment.saolourenco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaolourencoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaolourencoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmidevelopment/saolourenco/SaolourencoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmidevelopment.saolourenco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaolourencoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
