b0ade16579c1c80e9dea6f02302857d329669c20
Thiago Freitas <tfreitasdevv@gmail.com>
2021-02-22 18:18:42 -0300

    Iniciando tratamento de erros

--- src/main/java/com/tmidevelopment/saolourenco/services/EstadoService.java
+++ src/main/java/com/tmidevelopment/saolourenco/services/EstadoService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.tmidevelopment.saolourenco.domain.Estado;
 import com.tmidevelopment.saolourenco.repositories.EstadoRepository;
+import com.tmidevelopment.saolourenco.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class EstadoService {
 
 	@Autowired
 	private EstadoRepository repo;
-	
+
 	public Estado findById(Integer id) {
 		Optional<Estado> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
