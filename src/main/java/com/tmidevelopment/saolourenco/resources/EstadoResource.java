159760de4e1414627059427797a4625a8f4b159e
Thiago Freitas <tfreitasdevv@gmail.com>
2021-02-23 18:11:32 -0300

    DTO para cidades

--- src/main/java/com/tmidevelopment/saolourenco/resources/EstadoResource.java
+++ src/main/java/com/tmidevelopment/saolourenco/resources/EstadoResource.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tmidevelopment.saolourenco.domain.Cidade;
 import com.tmidevelopment.saolourenco.domain.Estado;
+import com.tmidevelopment.saolourenco.dto.CidadeDTO;
 import com.tmidevelopment.saolourenco.dto.EstadoDTO;
+import com.tmidevelopment.saolourenco.services.CidadeService;
 import com.tmidevelopment.saolourenco.services.EstadoService;
 
 @RestController
@@ -20,6 +23,8 @@ public class EstadoResource {
 
 	@Autowired
 	private EstadoService service;
+	@Autowired
+	private CidadeService cidadeService;
 
 	@GetMapping
 	public ResponseEntity<List<EstadoDTO>> findAll() {
@@ -32,6 +37,13 @@ public class EstadoResource {
 	public ResponseEntity<?> findById(@PathVariable Integer id) {
 		Estado obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping(value = "/{estadoId}/cidades")
+	public ResponseEntity<List<CidadeDTO>> findCidades(@PathVariable Integer estadoId) {
+		List<Cidade> list = cidadeService.findByEstado(estadoId);
+		List<CidadeDTO> listDTO = list.stream().map(obj -> new CidadeDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
