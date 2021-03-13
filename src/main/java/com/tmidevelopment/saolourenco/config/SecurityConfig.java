3072249b514639cc5ad4f0716c026ec40d2b6efb
Thiago Freitas <tfreitasdevv@gmail.com>
2021-03-12 22:18:46 -0300

    tentando resolver problema heroku 403

--- src/main/java/com/tmidevelopment/saolourenco/config/SecurityConfig.java
+++ src/main/java/com/tmidevelopment/saolourenco/config/SecurityConfig.java
@@ -80,10 +80,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
 
